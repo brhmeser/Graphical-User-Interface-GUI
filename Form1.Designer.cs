6141b77813abf48e80cb4cc82ffcea5ca297e03b
İbrahim Eser <ibrahim.esrr1@gmail.com>
2022-03-20 23:46:39 +0300

    GUI with C#
    The inputs received through the GUI
    using C#, Windows forms and related
    controls and the outputs to be created
    are presented to users by the GUI.

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace PROJE
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode51 = new System.Windows.Forms.TreeNode("UCGEN MI ?                              .    ");
+            System.Windows.Forms.TreeNode treeNode52 = new System.Windows.Forms.TreeNode("KARE MI ?                              .    ");
+            System.Windows.Forms.TreeNode treeNode53 = new System.Windows.Forms.TreeNode("KAGIT TAS MAKAS OYUNU                              .    ");
+            System.Windows.Forms.TreeNode treeNode54 = new System.Windows.Forms.TreeNode("ALTDIZGI MI ?                              .    ");
+            System.Windows.Forms.TreeNode treeNode55 = new System.Windows.Forms.TreeNode("ON EK VEYA ART EK MI ?                              .    ");
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.treeView1.ItemHeight = 85;
+            this.treeView1.Location = new System.Drawing.Point(46, 45);
+            this.treeView1.Name = "treeView1";
+            treeNode51.Name = "Düğüm0";
+            treeNode51.NodeFont = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            treeNode51.Text = "UCGEN MI ?                              .    ";
+            treeNode52.Name = "Düğüm1";
+            treeNode52.NodeFont = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            treeNode52.Text = "KARE MI ?                              .    ";
+            treeNode53.Name = "Düğüm2";
+            treeNode53.NodeFont = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            treeNode53.Text = "KAGIT TAS MAKAS OYUNU                              .    ";
+            treeNode54.Name = "Düğüm3";
+            treeNode54.NodeFont = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            treeNode54.Text = "ALTDIZGI MI ?                              .    ";
+            treeNode55.Name = "Düğüm4";
+            treeNode55.NodeFont = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            treeNode55.Text = "ON EK VEYA ART EK MI ?                              .    ";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode51,
+            treeNode52,
+            treeNode53,
+            treeNode54,
+            treeNode55});
+            this.treeView1.Size = new System.Drawing.Size(404, 466);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(685, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 89);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "UCGEN MI ?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(603, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(67, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(727, 188);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(67, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(853, 188);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(67, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(724, 320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.MediumOrchid;
+            this.label3.Location = new System.Drawing.Point(724, 407);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "SONUC";
+            this.label3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(688, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 54);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "KONTROL ET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Maroon;
+            this.button2.Location = new System.Drawing.Point(873, 483);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 39);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Formu Sonlandır";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 580);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Name = "Form1";
+            this.Text = "Q";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PROJE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (treeView1.SelectedNode.Name == "Düğüm0")
+            {
+                label1.Text = "UCGEN MI ?";
+                label3.Text = "SONUC";
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                textBox3.Visible = true;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                
+            }
+            if (treeView1.SelectedNode.Name == "Düğüm1")
+            {
+                label1.Text = "KARE MI ?";
+                label3.Text = "SONUC";
+                textBox1.Visible = true;
+                textBox2.Visible = false;
+                textBox3.Visible = true;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+            if (treeView1.SelectedNode.Name == "Düğüm2")
+            {
+                label1.Text = "KAGIT TAS MAKAS OYUNU";
+                label3.Text = "SONUC";
+                textBox1.Visible = true;
+                textBox2.Visible = false;
+                textBox3.Visible = true;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+            if (treeView1.SelectedNode.Name == "Düğüm3")
+            {
+                label1.Text = "ALTDIZGI MI ?";
+                label3.Text = "SONUC";
+                textBox1.Visible = true;
+                textBox2.Visible = false;
+                textBox3.Visible = true;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+            if (treeView1.SelectedNode.Name == "Düğüm4")
+            {
+                label1.Text = "ON EK VEYA ART EK MI ?";
+                label3.Text = "SONUC";
+                textBox1.Visible = true;
+                textBox2.Visible = false;
+                textBox3.Visible = true;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string fileName = @"E:/cikti.txt";
+
+            File.Delete(fileName);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (treeView1.SelectedNode.Name == "Düğüm0")
+            {
+              int a, b, c;
+                
+              a = int.Parse(textBox1.Text);
+              b = int.Parse(textBox2.Text);
+              c = int.Parse(textBox3.Text);
+                if (a > 0 && b > 0 && c > 0)
+                {
+                    if ((a - b < c) && (a + b > c) && (a - c < b) && (a + c > b) && (b - c < a) && (b + c > a))
+                {
+                    label3.Text = "UCGEN";
+                }
+                else
+                    label3.Text = "UCGEN DEGIL";
+                }
+                else
+                    MessageBox.Show("GIRDI HATASI");
+                kaydet();
+            }
+
+            if (treeView1.SelectedNode.Name == "Düğüm1")
+            {
+                int a, b;
+                a = int.Parse(textBox1.Text);
+                b = int.Parse(textBox3.Text);
+                if (a > 0 && b > 0) { 
+                     if (a*a!=b)
+                        label3.Text = "KARE DEGIL";
+                    else
+                        label3.Text = "KARE";
+                }
+                else
+                    MessageBox.Show("GIRDI HATASI");
+                kaydet();
+            }
+
+            if (treeView1.SelectedNode.Name == "Düğüm2")
+            {
+                // oyun verilen örnek koşmalardaki gibi türkçe karakterlere duyarlı değildir.
+
+                if (textBox1.Text.ToLower() == "makas" & textBox3.Text.ToLower() == "tas")
+                {
+                    label3.Text = "YENEMEDI";
+                }
+                else if (textBox1.Text.ToLower() == "makas" & textBox3.Text.ToLower() == "kagit")
+                {
+                    label3.Text = "YENDI";
+                }
+                else if (textBox1.Text.ToLower() == "kagit" & textBox3.Text.ToLower() == "tas")
+                {
+                    label3.Text = "YENDI";
+                }
+                else if (textBox1.Text.ToLower() == "tas" & textBox3.Text.ToLower() == "makas")
+                {
+                    label3.Text = "YENDI";
+                }
+                else if (textBox1.Text.ToLower() == "kagit" & textBox3.Text.ToLower() == "makas")
+                {
+                    label3.Text = "YENEMEDI";
+                }
+                else if (textBox1.Text.ToLower() == "tas" & textBox3.Text.ToLower() == "kagit")
+                {
+                    label3.Text = "YENEMEDI";
+                }
+                else if (textBox1.Text.ToLower() == textBox3.Text.ToLower())
+                    label3.Text = "YENEMEDI";
+                kaydet();
+               
+            }
+            if (treeView1.SelectedNode.Name == "Düğüm3")
+            {
+                bool dogrumu = false;
+                for (int i = 0; i < textBox3.Text.Length; i++)
+                {
+                    if (i < textBox3.Text.Length - textBox1.Text.Length+1)
+                    { 
+                     
+                        if (textBox1.Text == textBox3.Text.Substring(i, textBox1.Text.Length))
+                            dogrumu = true;
+                    }
+                }            
+                if (dogrumu)
+                    label3.Text = "ALTDIZGI";
+                else
+                    label3.Text = "ALTDIZGI DEGIL";
+                kaydet();
+            }
+
+            if (treeView1.SelectedNode.Name == "Düğüm4")
+            {
+                bool dogrumu = true;
+
+                    if (textBox1.Text == textBox3.Text.Substring(textBox3.Text.Length - textBox1.Text.Length))
+                    { 
+                        label3.Text = "ART EK";
+                          dogrumu = false;
+                    }
+    
+                    if (textBox1.Text == textBox3.Text.Substring(0, textBox1.Text.Length)) 
+                    { 
+                        label3.Text = "ON EK";
+                        dogrumu = false;
+                    }
+                    if(dogrumu)
+                    label3.Text = "HICBIRI DEGIL";
+                    
+                kaydet();
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult sonuc;
+            sonuc = MessageBox.Show("Çıkmak İstediğinizden Emin misiniz ?", "Çıkış", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);        
+            if (sonuc == DialogResult.Yes)
+            {
+                
+                this.Close();
+                Application.Exit();
+            }
+        }
+        void kaydet()
+        {
+            string fileName = @"E:/cikti.txt";
+                       
+            FileStream fs = new FileStream(fileName, FileMode.Append, FileAccess.Write, FileShare.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            if (treeView1.SelectedNode.Name == "Düğüm0")
+            
+              sw.WriteLine("" + label1.Text + " : " + textBox1.Text + " " + textBox2.Text + " " + textBox3.Text);
+           
+            else
+              sw.WriteLine("" + label1.Text + " : " + textBox1.Text  + " " + textBox3.Text);
+              sw.WriteLine("" + label3.Text);
+              sw.Close();
+
+        }
+    }
+}
